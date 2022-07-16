a0cc7efb07fe0415690905ced1c5defc4609a8d9
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2022-07-16 11:45:42 -0400

    split files

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zerologr"
@@ -27,99 +26,12 @@ func run() error {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	if err := prunePackages(ctx, log, client.Organizations, "thepwagner-org"); err != nil {
-		return err
-	}
 	if err := prunePackages(ctx, log, client.Users, "thepwagner"); err != nil {
 		return err
 	}
-	return nil
-}
-
-type packageClient interface {
-	ListPackages(ctx context.Context, org string, opts *github.PackageListOptions) ([]*github.Package, *github.Response, error)
-	PackageGetAllVersions(ctx context.Context, org, packageType, packageName string, opts *github.PackageListOptions) ([]*github.PackageVersion, *github.Response, error)
-	PackageDeleteVersion(ctx context.Context, org, packageType, packageName string, id int64) (*github.Response, error)
-}
-
-var _ packageClient = (*github.OrganizationsService)(nil)
-var _ packageClient = (*github.UsersService)(nil)
-
-func prunePackages(ctx context.Context, log logr.Logger, client packageClient, org string) error {
-	packages, _, err := client.ListPackages(ctx, org, &github.PackageListOptions{PackageType: github.String("container")})
-	if err != nil {
-		return fmt.Errorf("failed to list packages: %w", err)
+	if err := prunePackages(ctx, log, client.Organizations, "thepwagner-org"); err != nil {
+		return err
 	}
-	log.Info("listed packages", "package_count", len(packages))
-
-	for _, pkg := range packages {
-		pkgLog := log.WithValues("package_name", *pkg.Name)
-		pkgLog.Info("processing package", "package_id", *pkg.ID, "version_count", pkg.GetVersionCount())
-		versions, _, err := client.PackageGetAllVersions(ctx, org, "container", *pkg.Name, &github.PackageListOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to list versions of %q: %w", *pkg.Name, err)
-		}
-		pkgLog.Info("listed versions", "version_count", len(versions))
-
-		var latestDigest string
-	firstPass:
-		for _, v := range versions {
-			ctr := v.GetMetadata().GetContainer()
-			if ctr == nil {
-				continue
-			}
-
-			for _, t := range ctr.Tags {
-				if t == "latest" {
-					latestDigest = *v.Name
-					continue firstPass
-				}
-			}
-
-			if len(ctr.Tags) == 0 {
-				pkgLog.Info("deleting untagged version", "version_id", *v.ID)
-				_, err := client.PackageDeleteVersion(ctx, org, "container", *pkg.Name, *v.ID)
-				if err != nil {
-					return fmt.Errorf("failed to delete version %q %d: %w", *pkg.Name, *v.ID, err)
-				}
-			}
-		}
-
-		if latestDigest == "" {
-			continue
-		}
-
-		versions, _, err = client.PackageGetAllVersions(ctx, org, "container", *pkg.Name, &github.PackageListOptions{})
-		if err != nil {
-			return fmt.Errorf("failed to list versions of %q: %w", *pkg.Name, err)
-		}
-		pkgLog.Info("listed versions", "version_count", len(versions))
-
-		digest := strings.Replace(latestDigest, ":", "-", -1)
-		latestAtt := fmt.Sprintf("%s.att", digest)
-		latestSig := fmt.Sprintf("%s.sig", digest)
-	secondPass:
-		for _, v := range versions {
-			ctr := v.GetMetadata().GetContainer()
-			if ctr == nil {
-				continue
-			}
-
-			for _, t := range ctr.Tags {
-				switch t {
-				case latestAtt, latestSig, "latest":
-					continue secondPass
-				}
-			}
-
-			pkgLog.Info("deleting stale version", "version_id", *v.ID)
-			_, err := client.PackageDeleteVersion(ctx, org, "container", *pkg.Name, *v.ID)
-			if err != nil {
-				return fmt.Errorf("failed to delete version %q %d: %w", *pkg.Name, *v.ID, err)
-			}
-		}
-	}
-
 	return nil
 }
 
---
+++ prune.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	"github.com/google/go-github/v43/github"
+)
+
+type packageClient interface {
+	ListPackages(ctx context.Context, org string, opts *github.PackageListOptions) ([]*github.Package, *github.Response, error)
+	PackageGetAllVersions(ctx context.Context, org, packageType, packageName string, opts *github.PackageListOptions) ([]*github.PackageVersion, *github.Response, error)
+	PackageDeleteVersion(ctx context.Context, org, packageType, packageName string, id int64) (*github.Response, error)
+}
+
+var _ packageClient = (*github.OrganizationsService)(nil)
+var _ packageClient = (*github.UsersService)(nil)
+
+func prunePackages(ctx context.Context, log logr.Logger, client packageClient, org string) error {
+	packages, _, err := client.ListPackages(ctx, org, &github.PackageListOptions{PackageType: github.String("container")})
+	if err != nil {
+		return fmt.Errorf("failed to list packages: %w", err)
+	}
+	log.Info("listed packages", "package_count", len(packages))
+
+	for _, pkg := range packages {
+		pkgLog := log.WithValues("package_name", *pkg.Name)
+		pkgLog.Info("processing package", "package_id", *pkg.ID, "version_count", pkg.GetVersionCount())
+		versions, _, err := client.PackageGetAllVersions(ctx, org, "container", *pkg.Name, &github.PackageListOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to list versions of %q: %w", *pkg.Name, err)
+		}
+		pkgLog.Info("listed versions", "version_count", len(versions))
+
+		var latestDigest string
+	firstPass:
+		for _, v := range versions {
+			ctr := v.GetMetadata().GetContainer()
+			if ctr == nil {
+				continue
+			}
+
+			for _, t := range ctr.Tags {
+				if t == "latest" {
+					latestDigest = *v.Name
+					continue firstPass
+				}
+			}
+
+			if len(ctr.Tags) == 0 {
+				pkgLog.Info("deleting untagged version", "version_id", *v.ID)
+				_, err := client.PackageDeleteVersion(ctx, org, "container", *pkg.Name, *v.ID)
+				if err != nil {
+					return fmt.Errorf("failed to delete version %q %d: %w", *pkg.Name, *v.ID, err)
+				}
+			}
+		}
+
+		if latestDigest == "" {
+			continue
+		}
+
+		versions, _, err = client.PackageGetAllVersions(ctx, org, "container", *pkg.Name, &github.PackageListOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to list versions of %q: %w", *pkg.Name, err)
+		}
+		pkgLog.Info("listed versions", "version_count", len(versions))
+
+		digest := strings.Replace(latestDigest, ":", "-", -1)
+		latestAtt := fmt.Sprintf("%s.att", digest)
+		latestSig := fmt.Sprintf("%s.sig", digest)
+	secondPass:
+		for _, v := range versions {
+			ctr := v.GetMetadata().GetContainer()
+			if ctr == nil {
+				continue
+			}
+
+			for _, t := range ctr.Tags {
+				switch t {
+				case latestAtt, latestSig, "latest":
+					continue secondPass
+				}
+			}
+
+			pkgLog.Info("deleting stale version", "version_id", *v.ID)
+			_, err := client.PackageDeleteVersion(ctx, org, "container", *pkg.Name, *v.ID)
+			if err != nil {
+				return fmt.Errorf("failed to delete version %q %d: %w", *pkg.Name, *v.ID, err)
+			}
+		}
+	}
+
+	return nil
+}
